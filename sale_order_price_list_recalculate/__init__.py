493eba810b7130073fad012591ab4c37512b679d
Manish Bohra <manish.bohra@sunarctechnologies.com>
2020-11-01 20:11:33 +0530

    initial commit

---
+++ sale_order_price_list_recalculate/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2020 Manish Kumar Bohra <manishbohra1994@gmail.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import models
---
+++ sale_order_price_list_recalculate/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Manish Kumar Bohra <manishbohra1994@gmail.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Sale Price Recalculate',
+    'version': '1.0',
+    'summary': 'This module manage to product price list re-calculation',
+    'description': 'In this module added a new feature related to the re-calculate price list in the sales order',
+    'category': 'Other',
+    'author': 'Manish Bohra',
+    'website': 'manishbohra1994@gmail.com',
+    'license': 'AGPL-3',
+    'depends': ['sale'],
+    'installable': True,
+    'auto_install': False,
+}
