46722af1b0be8d39bdab8597ef3fbde4874ec1ba
Manish Bohra <manish.bohra@sunarctechnologies.com>
2020-11-01 22:52:31 +0530

    added changes related issues

--- sale_order_price_list_recalculate/models/__init__.py
+++ sale_order_price_list_recalculate/models/__init__.py
@@ -1,4 +1,5 @@
-# Copyright 2020 Manish Kumar Bohra <manishbohra1994@gmail.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# Copyright 2020 Manish Kumar Bohra <manishbohra1994@gmail.com> or <manishkumarbohra@outlook.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/Lgpl-3.0.html
+
 
 from . import sale
